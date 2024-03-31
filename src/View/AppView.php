2ae6597c07d73208a61af5ba56bf38d9edf27585
Danilo Aires <airesdanilo@gmail.com>
2024-03-31 18:12:19 -0300

    instalado plugin cakelte

--- src/View/AppView.php
+++ src/View/AppView.php
@@ -16,26 +16,12 @@ declare(strict_types=1);
 namespace App\View;
 
 use Cake\View\View;
+use CakeLte\View\CakeLteView;
 
-/**
- * Application View
- *
- * Your application's default view class
- *
- * @link https://book.cakephp.org/4/en/views.html#the-app-view
- */
-class AppView extends View
-{
-    /**
-     * Initialization hook method.
-     *
-     * Use this method to add common initialization code like loading helpers.
-     *
-     * e.g. `$this->loadHelper('Html');`
-     *
-     * @return void
-     */
-    public function initialize(): void
-    {
-    }
+class AppView extends CakeLteView{
+
+  public function initialize(): void{
+    parent::initialize();
+    //...
+  }
 }
