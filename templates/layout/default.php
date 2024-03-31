c3999235ff90b65539db1e8b035b5783facfb337
Danilo Aires <airesdanilo@gmail.com>
2024-03-31 17:53:20 -0300

    tudo ok com acl

--- templates/layout/default.php
+++ templates/layout/default.php
@@ -39,8 +39,7 @@ $cakeDescription = 'CakePHP: the rapid development php framework';
             <a href="<?= $this->Url->build('/') ?>"><span>Cake</span>PHP</a>
         </div>
         <div class="top-nav-links">
-            <a target="_blank" rel="noopener" href="https://book.cakephp.org/4/">Documentation</a>
-            <a target="_blank" rel="noopener" href="https://api.cakephp.org/">API</a>
+            <?= $this->Html->link('Logout', ['controller' => 'Users', 'action' => 'logout']) ?>
         </div>
     </nav>
     <main class="main">
