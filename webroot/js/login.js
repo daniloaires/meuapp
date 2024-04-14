69a7c8dfd942b585dad8a5865677327308c288cd
Danilo Aires <airesdanilo@gmail.com>
2024-04-14 10:37:03 -0300

    ok

--- webroot/js/login.js
+++ webroot/js/login.js
@@ -1,14 +1,3 @@
 $(document).ready(function() {
-    // Exibe as mensagens flash com SweetAlert
-    $('.sweetalert').each(function() {
-        var type = $(this).data('type');
-        var message = $(this).text();
 
-        swal.fire({
-            icon: type,
-            title: message,
-            showConfirmButton: false,
-            timer: 3000 // Fecha automaticamente após 3 segundos
-        });
-    });
 });
