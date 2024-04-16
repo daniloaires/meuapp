6f26dd257e2fe3e48c80b07bf5b2405123ae4aff
Danilo Aires <airesdanilo@gmail.com>
2024-04-16 20:01:34 -0300

    mais alguns ajustes

--- templates/element/flash/error.php
+++ templates/element/flash/error.php
@@ -1,11 +1,19 @@
 <?php
-/**
- * @var \App\View\AppView $this
- * @var array $params
- * @var string $message
- */
 if (!isset($params['escape']) || $params['escape'] !== false) {
     $message = h($message);
 }
 ?>
-<div class="message error" onclick="this.classList.add('hidden');"><?= $message ?></div>
+<script>
+  setTimeout(function () {
+        Swal.fire({
+            title: "<?= __('Erro!') ?>",
+            icon: 'error',
+            html: '<?= html_entity_decode($message) ?>',
+            showCloseButton: false,
+            allowOutsideClick: true,
+            heightAuto: false,
+            confirmButtonText:
+                'Ok',
+        });
+    }, 200);
+</script>
