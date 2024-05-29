8f8f35283c1b290b27823aa8b32dea639113c087
Danilo Aires <airesdanilo@gmail.com>
2024-05-29 11:21:25 -0300

    ok

--- templates/Orders/edit.php
+++ templates/Orders/edit.php
@@ -12,31 +12,30 @@
                 <legend><?= __('Pedido: ' . h($order->nome)) ?></legend>
             </fieldset>
 
-            <h6><?= __('Itens do Pedido') ?></h6>
-
             <div class="row">
-                <div class="col mb-8">
-                    <label for="product-id"><?= __('Product ID') ?></label>
-                    <input type="number" id="product-id" class="form-control" placeholder="Product ID">
+                <div class="col-md-8">
+                    <label for="product-id"><?= __('Produto') ?></label>
+                    <input type="text" id="product-id" class="form-control" placeholder="Product ID">
                 </div>
-                <div class="col mb-2">
+                <div class="col-md-2">
                     <label for="qtde"><?= __('Quantidade') ?></label>
                     <input type="number" id="qtde" class="form-control" placeholder="Quantidade">
                 </div>
 
-                <div class="col mb-2"><br />
+                <div class="col-md-2"><br />
                     <button id="add-item" class="btn btn-primary mb-2">
                         <?= __('Adicionar Item') ?>
                     </button>
                 </div>                
-            </div>
+            </div><hr />
 
+            <h6><?= __('Itens do Pedido') ?></h6>
             <div class="table-responsive">
                 <table class="table table-bordered table-striped">
                     <thead>
                         <tr>
                             <th class='nowrap'><?= __('ID') ?></th>
-                            <th class='nowrap'><?= __('Product ID') ?></th>
+                            <th class='nowrap'><?= __('Produto') ?></th>
                             <th class='nowrap'><?= __('Quantidade') ?></th>
                             <th class='nowrap'><?= __('Valor') ?></th>
                         </tr>
