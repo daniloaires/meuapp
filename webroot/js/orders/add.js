787f87737387e44d0743e9cd86f116732701ab92
Danilo Aires <airesdanilo@gmail.com>
2024-05-29 13:10:15 -0300

    ok

--- webroot/js/orders/add.js
+++ webroot/js/orders/add.js
@@ -10,7 +10,7 @@ jQuery(function() {
                     term: request.term
                 },
                 success: function(data) {
-                    response($.map(data, function(item) {
+                    response($.map(data.result, function(item) {
                         return {
                             label: item.nome + ' (' + item.id + ')',
                             value: item.nome,
@@ -24,8 +24,56 @@ jQuery(function() {
         select: function(event, ui) {
             $('#product-id').val(ui.item.id);
             $('#product-search').val(ui.item.value);
-            $('#product-valor').val(ui.item.valor); // Certifique-se de adicionar este campo no HTML se precisar usá-lo
+            $('#product-valor').val(ui.item.valor);
             return false;
+        }
+    });
+
+    // Evento ao clicar no botão "Adicionar Item"
+    $('#add-item').on('click', function(e){
+        e.preventDefault();
+
+        // Coleta os dados do formulário
+        var productId = $('#product-id').val();
+        var qtde = $('#qtde').val();
+        var valor = $('#product-valor').val();
+
+        if (productId && qtde && valor) {
+            var total = qtde * valor;
+
+            // Envia a requisição AJAX para adicionar o item
+            $.ajax({
+                url: '/orders/addOrderItem',  // URL direta
+                type: 'POST',
+                data: {
+                    //order_id: <?= $order->id ?>,  // Passa o order_id diretamente
+                    product_id: productId,
+                    qtde: qtde,
+                    valor: total
+                },
+                success: function(response) {
+                    if (response.success) {
+                        // Adiciona o novo item na tabela
+                        $('#order-items-table').append(
+                            '<tr>' +
+                            '<td>' + response.orderItem.id + '</td>' +
+                            '<td>' + response.orderItem.product_id + '</td>' +
+                            '<td>' + response.orderItem.qtde + '</td>' +
+                            '<td>' + response.orderItem.valor + '</td>' +
+                            '</tr>'
+                        );
+
+                        // Limpa os campos do formulário
+                        $('#product-id').val('');
+                        $('#product-search').val('');
+                        $('#qtde').val('');
+                    } else {
+                        alert('Erro ao adicionar item');
+                    }
+                }
+            });
+        } else {
+            alert('Por favor, preencha todos os campos.');
         }
     });
             
